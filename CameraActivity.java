22315d87a38953cf25fe70a4e746874d5ed03d88
Charlie5577 <63338308+Charlie5577@users.noreply.github.com>
2021-06-05 15:01:08 +0530

    Create CameraActivity.java

---
+++ CameraActivity.java
@@ -0,0 +1,28 @@
+private TextToSpeech textToSpeech;
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        LOGGER.d("onCreate " + this);
+        super.onCreate(null);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        setContentView(R.layout.activity_camera);
+
+        if (hasPermission()) {
+            setFragment();
+        } else {
+            requestPermission();
+        }
+
+        this.textToSpeech = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status == TextToSpeech.SUCCESS) {
+                    LOGGER.i("onCreate", "TextToSpeech is initialised");
+                } else {
+                    LOGGER.e("onCreate", "Cannot initialise text to speech!");
+                }
+            }
+        });
+
+    }
